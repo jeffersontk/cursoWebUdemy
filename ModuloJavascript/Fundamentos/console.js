559991bc82489e3e23ea9b04394def73c154f831
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-11-18 16:34:22 -0300

    Create console.js
    alguns comandos console

---
+++ ModuloJavascript/Fundamentos/console.js
@@ -0,0 +1,12 @@
+//alguns comandos console
+
+// envia uma mensagem para o console
+console.log("Olá mundo")
+//Envia uma mensagem quando expression é avaliado como falso
+console.assert((x == 1), "assert message: x != 1");
+//limpa as mensagens do console
+console.clear()
+//enviar uma mensagem de error
+console.error('deu ruim')
+
+// para mais comandos entrar em https://docs.microsoft.com/pt-br/visualstudio/debugger/javascript-console-commands?view=vs-2017
