bf0008d0dcbef2c816ff1c0a1902591f7f98fa6d
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-11-18 19:18:17 -0300

    objetos

---
+++ ModuloJavascript/Fundamentos/aula2/objetos.js
@@ -0,0 +1,21 @@
+const prod1 = {}
+
+prod1.nome = 'celular axt2030'
+
+prod1.preco = '1999.9'
+
+prod1['desconto'] = 0.49
+
+console.log(prod1);
+
+const cliente = {
+    nome: 'jeff',
+    idade: 23,
+    obj: {
+        blabla: 1,
+        obj: {
+            blablaa: 2
+        }
+    }
+}
+console.log(cliente);
