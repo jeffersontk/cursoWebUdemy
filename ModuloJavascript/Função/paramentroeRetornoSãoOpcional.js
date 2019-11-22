6f506acb5ef0ca6fd949dc32205b12477fe70308
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-11-22 17:19:00 -0300

    params Variaveis

---
+++ ModuloJavascript/Função/paramentroeRetornoSãoOpcional.js
@@ -0,0 +1,14 @@
+function area (largura, altura){
+    let area = largura * altura
+    if( area >= 20){
+        console.log(`valor acima do permitido, ${area}m²`)
+    }else{
+        return area
+    }
+}
+
+console.log(area(2,2))
+console.log(area(2))
+console.log(area())
+console.log(area(2,3,4,5,6,7))
+console.log(area(5,5))
