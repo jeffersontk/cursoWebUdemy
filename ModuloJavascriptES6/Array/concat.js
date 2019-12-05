229b69f4a411138a44e60111ecc14096576a5899
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-12-05 01:13:55 -0300

    concat

---
+++ ModuloJavascriptES6/Array/concat.js
@@ -0,0 +1,9 @@
+const filhas = ['Janny', 'Rachel']
+const filhos = ['Bruce', 'Clark']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos)
+console.log(filhas)
+console.log(filhos)
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [
+    [6, 7]
+]));
