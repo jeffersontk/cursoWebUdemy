ff4952039b1399cb0c99d8e23b8dd33b6d35f819
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-12-09 02:03:20 -0300

    Express

---
+++ ModuloNodeJS/projetoExpress/src/server.js
@@ -0,0 +1,21 @@
+const porta = 3003;
+
+const express = require("express");
+
+const app = express();
+
+app.get("/produtos", (req, res, next) => {
+  console.log("Middleware 1..");
+  next();
+});
+
+app.get("/produtos", (req, res, next) => {
+  res.send({
+    nome: "notebook",
+    preco: 123.45
+  });
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor executando na porta ${porta}`);
+});
