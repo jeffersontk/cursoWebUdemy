767ca7b296a302159f68b36b09ce0aadbd92271a
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-12-09 23:48:03 -0300

    EsNext revisão
    taggedTemplates
    Map
    Set
    forOf

---
+++ ModuloESNext/taggedTemplate1.js
@@ -0,0 +1,10 @@
+//Tagged templates - processa o template dentro de uma função
+function tag(partes, ...valores) {
+    console.log(partes);
+    console.log(valores);
+    return 'Outra string'
+}
+
+const aluno = 'Andrey'
+const situacao = 'aprovado'
+console.log(tag `${aluno} esta ${situacao}`);
