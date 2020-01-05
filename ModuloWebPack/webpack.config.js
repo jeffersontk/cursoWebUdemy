2f3fd6329ef6c97c6cf98e7a49de9cd39bd8c48a
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-04 23:58:34 -0300

    webpack scss

--- ModuloWebPack/webpack.config.js
+++ ModuloWebPack/webpack.config.js
@@ -1,11 +1,25 @@
+const modeDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+
 module.exports = {
-    mode: 'development',
+    mode: modeDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -14,11 +28,12 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
                 //'style-loader', //adicionar css a dom injetando a tag <style>
                 'css-loader', //interpreta @import, url()
+                'sass-loader',
             ]
         }]
     }
