7d51097f232ec7f2d8da97e6c767edb04daf17bd
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-11 12:30:30 -0300

    update logo do projeto

--- ModuloReactJS/crud/frontend/src/components/templates/Logo.jsx
+++ ModuloReactJS/crud/frontend/src/components/templates/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Logo.css'
-import logo from '../../assets/imgs/logo.png'
+import logo from '../../assets/imgs/logo2.png'
 
 export default props =>
     <aside className="logo">
         <a href="/" className="logo">
-            <img src={logo} alt="logo" />
+            <img className="img-fluid" src={logo} alt="logo" />
         </a>
     </aside>
